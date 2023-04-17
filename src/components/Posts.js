98e6e8e9709682f268121198180fa797ff3f9f0d
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-04-17 10:21:39 -0700

    add screenshot

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,2 +1,17 @@
 import React from "react";
-import 
+import PropTypes from "prop-types";
+
+function Posts(props){
+  return (
+    <React.Fragment>
+      <h3>{props.userName}</h3>
+    </React.Fragment>
+  )
+}
+
+Posts.propTypes = {
+  userName: propTypes.string.isRequired,
+  postText: PropTypes.string.isRequired
+}
+
+export default Posts;
