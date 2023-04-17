3d4ae97072e4b31737eea3f22e53e5893ab04b17
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-04-17 13:02:35 -0700

    rename parent folder

--- src/components/UserBio.js
+++ src/components/UserBio.js
@@ -8,7 +8,7 @@ function UserBio(props){
         <hr/>
         <aside>
         <h3>{props.userName}</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer enim neque volutpat ac tincidunt vitae semper quis lectus. Ornare arcu dui vivamus arcu felis bibendum ut tristique et. Nunc consequat interdum varius sit amet. Varius morbi enim nunc faucibus a pellentesque. Nullam ac tortor vitae purus faucibus. Augue neque gravida in fermentum et sollicitudin ac.</p>
+        <p>UserBio: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer enim neque volutpat ac tincidunt vitae semper quis lectus. Ornare arcu dui vivamus arcu felis bibendum ut tristique et. Nunc consequat interdum varius sit amet. Varius morbi enim nunc faucibus a pellentesque. Nullam ac tortor vitae purus faucibus. Augue neque gravida in fermentum et sollicitudin ac.</p>
         </aside>
       </div>
     </React.Fragment>
